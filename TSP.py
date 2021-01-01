9d14035e667d8eedd3ff7e6a0bf63bc63b61c4f0
padj <50206208+padj@users.noreply.github.com>
2021-01-01 11:41:00 +0000

    Add files via upload

---
+++ TSP.py
@@ -0,0 +1,202 @@
+## 
+# Bespoke module created to solve the 2D generalised travelling salesman 
+# problem via a genetic algorithm, generalised to allow for comparisons of 
+# GA parameters.
+
+# Future additions to add multiple selection and crossover processes. 
+
+#%% PREAMBLE
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+#%% FUNCTION DEFINITIONS
+
+def makeTowns(nTowns, xMin, xMax, yMin, yMax, RNG):
+    towns = []
+    if RNG == False:
+        random.seed(a=1)
+    for i in range(nTowns):
+        towns.append([random.uniform(xMin, xMax), random.uniform(yMin, yMax)])
+    random.seed()
+    return towns  
+    
+def createPopulation(numberOfTowns, popSize):
+    population = []
+    sampleList = range(numberOfTowns)
+    for i in range(popSize):
+        population.append(random.sample(sampleList, numberOfTowns))
+    return population
+
+
+def calcFitness(idv, towns):
+    # fitness is the cumulative Euclidean distance between each city in the 
+    # route order, including the distance between the last and first cities.
+    fitness = 0 #initialise fitness
+    for i in range(len(idv)-1):
+        xDiff = towns[idv[i]][0] - towns[idv[i+1]][0]
+        yDiff = towns[idv[i]][1] - towns[idv[i+1]][1]
+        fitness += np.sqrt(xDiff**2 + yDiff**2)
+    # Add length from last city, back to first city
+    xDiff = towns[idv[0]][0] - towns[idv[-1]][0]
+    yDiff = towns[idv[0]][1] - towns[idv[-1]][1]
+    fitness += np.sqrt(xDiff**2 + yDiff**2)
+    return fitness
+
+
+def determineFitnesses(population, towns):
+    fitnesses = []
+    for i in range(len(population)):
+        fitnesses.append(calcFitness(population[i], towns))
+    return fitnesses
+
+        
+def initialise(nTowns, nPop, xLowerBound, xUpperBound, yLowerBound, 
+                  yUpperBound, RNG):
+    towns = makeTowns(nTowns, xLowerBound, xUpperBound, yLowerBound, 
+                      yUpperBound, RNG) # create towns
+    
+    population = createPopulation(nTowns, nPop) # create initial population
+    
+    fitnesses = determineFitnesses(population, towns) # determine fitnesses 
+    # of initial population
+    
+    return [towns, population, fitnesses]
+
+
+def elitism(population, fitnessList, eliteRate):
+    A = list(range(len(population))) # create range of population
+    zipped = zip(fitnessList, A) # zip together pop range and fitnesses
+    B = sorted(zipped) # sort by fitnesses
+    topNum = int(np.round(eliteRate*len(population)))
+    elites = []
+    others = []
+    for i in range(topNum):
+        elites.append(population[B[i][1]])
+    for j in range(len(population)-topNum):
+        others.append(population[B[j][1]])
+    return [elites, others]
+ 
+
+def selection(others):
+    # 
+    selected = others
+    return selected
+
+
+def crossover(matingPool):
+    # single point crossover
+    newPop = []
+    for i in range(int(len(matingPool)/2)):
+        [parentA, parentB] = random.sample(matingPool,2) # pick parents
+        matingPool.remove(parentA) # remove parents from mating pool
+        matingPool.remove(parentB)
+        crossPoint = random.randint(0, len(parentA)-1) # pick crossover point.
+        
+        # Crossover for child A
+        childA = [len(parentA)+1]*len(parentA) # make child
+        childA[crossPoint:] = parentB[crossPoint:] # add in section from parent
+        childA[:crossPoint] = [gene for gene in parentA if gene not in childA]
+        # list comprehension, find genes in parentA no already in child, and 
+        # keep in original order, add them to the start of the child.
+        
+        # Repeat for child B
+        childB = [len(parentB)+1]*len(parentB) # make child
+        childB[crossPoint:] = parentA[crossPoint:] # add in section from parent
+        childB[:crossPoint] = [gene for gene in parentB if gene not in childB]
+        # list comprehension, find genes in parentA no already in child, and 
+        # keep in original order, add them to the start of the child.        
+                
+        newPop.append(childA) # Add children to new population
+        newPop.append(childB)
+    
+    return newPop
+
+def mutation(population, mutationRate):
+    newPop = population[:]
+    for i in range(len(population)): # For each individual in pop
+        if random.random() <= mutationRate: # decide if mutation occurs
+            a = random.randint(0, len(population[i])-1) # pick where mutation occurs 1
+            b = random.randint(0, len(population[i])-1) # pick second
+            
+            gene1 = population[i][a] # first city to swap
+            gene2 = population[i][b] # second city to swap
+            
+            newPop[i][a] = gene2 # swap cities
+            newPop[i][b] = gene1
+        
+    return newPop
+
+def runGA(population, towns, eliteRate, mutationRate, iterationMax, 
+          convergenceCriteria):
+    # Run the algorithm
+    bestIndividuals = []
+    bestFitnesses = []
+    globalBestFitness = [1e12]
+    residual, iteration = 0,0
+    while (residual > convergenceCriteria) + (iteration > iterationMax) == 0:
+        fitnesses = determineFitnesses(population=population[:], towns=towns) # determine fitnesses
+        
+        [elites, others] = elitism(population=population[:], 
+                                fitnessList=fitnesses, eliteRate=eliteRate) 
+        # determine elites & others where others = population - elites
+        
+        bestIndividuals.append(elites[0]) # Add best current individual to list
+        # Calculate and store the current best fitness for convergence.
+        currentBestFitness = determineFitnesses(
+                            population=[elites[0]], towns=towns)
+        bestFitnesses.append(currentBestFitness)
+        
+        selected = selection(others=others[:]) # carry out selection on others
+
+        crossed = crossover(matingPool=selected[:]) # carry out crossover on 
+        # selected individuals.
+        
+        newPop = elites + crossed # newPopulation is the elite individuals 
+        # plus the result of the crossover
+        
+        newPop = mutation(population=newPop[:], mutationRate=mutationRate) 
+        # carry out mutation on the new population
+        
+        # compare the last X values of the best individuals, where X is 
+        # controlled by convergenceCriteria. If the best fitnesses hasn't 
+        # changed assume converged.
+#        if len(bestIndividuals)>convergenceCriteria:
+#            lastX = bestIndividuals[-convergenceCriteria:]
+#            lastXF = determineFitnesses(population=lastX[:], towns=towns)
+#            if np.sum(lastXF/lastXF[-1]) == convergenceCriteria:
+#                residual = 1
+        if currentBestFitness < globalBestFitness:
+            residual = 0
+            globalBestFitness = currentBestFitness
+        else:
+            residual += 1
+        
+        population = newPop[:]
+        iteration += 1
+    
+    finalFitnesses = determineFitnesses(population=population[:], towns=towns)
+    return population, finalFitnesses, bestIndividuals, bestFitnesses
+
+def plotCitiesRoute(towns, individual, colour, figNum):
+    plt.figure(figNum)
+    i = 0
+    for town in towns:
+        plt.scatter(town[0],town[1],s=50, c='r', marker='H')
+        cityName = 'city%s'%i
+        plt.annotate(cityName, town)
+        i += 1
+    plt.grid('on')
+    plt.axis([0,100,0,100])
+    for i in range(len(individual)):
+        fromTown = individual[i]
+        if i == len(individual)-1:
+            toTown = individual[0]
+        else:
+            toTown = individual[i+1]
+        dx = towns[toTown][0]-towns[fromTown][0]
+        dy = towns[toTown][1]-towns[fromTown][1]
+        plt.arrow(towns[fromTown][0], towns[fromTown][1], dx, dy, color=colour)
+    
+    
+    
---
+++ TSP_run.py
@@ -0,0 +1,72 @@
+#%% Preamble
+
+import numpy as np
+import matplotlib.pyplot as plt
+import TSP 
+
+#%% Initialise
+# This section initialises the problem including number of towns in the wolrd, 
+# total GA population size, limits for location of the towns, and whether or
+# not to randomly determine the town locations (i.e RNG=False). Setting RNG to 
+# False allows for comparisons of the efficiency of GA parameters. This section
+# also plots an initial example route.
+
+total_number_towns = 10
+total_GA_population_size = 100
+town_location_limit_lowerX = 0
+town_location_limit_upperX = 100
+town_location_limit_lowerY = 0
+town_location_limit_upperY = 100
+
+[towns, pop, initialFitnesses] = TSP.initialise(
+                                nTowns=total_number_towns, 
+                                nPop=total_GA_population_size, 
+                                xLowerBound=town_location_limit_lowerX,
+                                xUpperBound=town_location_limit_upperX, 
+                                yLowerBound=town_location_limit_lowerY, 
+                                yUpperBound=town_location_limit_upperY, 
+                                RNG=False)
+
+TSP.plotCitiesRoute(towns=towns, individual=pop[0], colour='b', figNum=0)
+
+#%% RUN
+# This section runs the GA based on the towns, population, and fitnesses of 
+# the intialisation function and based on the input elitism rate (%), 
+# mutation rate (%), maximum number of generations, and the stagnation 
+# criteria. 
+
+elitism_rate = 0.1
+mutation_rate = 0.01
+maximum_number_generations = 1000
+stagnation_criteria = 100 # This represents the number of generations where
+# the best fitness of the population did not change in order to assume the 
+# algorithm has converged. 
+
+[finalPop, finalFitnesses, bestIdvs, bestFits] = TSP.runGA(
+                            population=pop[:], towns=towns, 
+                            eliteRate=elitism_rate, 
+                            mutationRate=mutation_rate, 
+                            iterationMax=maximum_number_generations, 
+                            convergenceCriteria=stagnation_criteria)
+
+#%% OUTPUTS
+# This section prints to console the total number of 
+# generations simulated, the initial best fitness, the best fitness of the 
+# final population, and the best global fitness found, and then plots the best
+# individual of the final population. This section also plots 
+# the convergence of the algorithm based on the best fitness of each 
+# population against the generation. 
+
+print('\n- Total number of generations = %s' %len(bestIdvs))
+print('- Best fitness of initial population = %s' %np.min(initialFitnesses))
+print('- Best fitness of final population = %s' %np.min(finalFitnesses))
+print('- Best fitness found during algorithm = %s' %np.min(bestFits))
+
+
+TSP.plotCitiesRoute(towns=towns, individual=bestIdvs[-1], colour='r', figNum=1)
+
+plt.figure(2)
+plt.plot(bestFits)
+plt.grid('on')
+plt.xlabel('Generation number')
+plt.ylabel('Best fitness per generation')
